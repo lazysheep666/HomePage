3b49758a148ccbebfa3a041ae92de34e1fc4c982
lazysheep <714201376@qq.com>
2018-11-13 17:24:37 +0800

    :tada: 0.0.1v

---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+import run from './RandomStr'
+import '../style/index.styl'
+
+let isRunning = false
+
+const init = (id) => {
+  listen(id)
+  play(id)
+}
+
+function listen(id) {
+  document.getElementById(id).addEventListener('click', function() {
+    if (isRunning) {
+      return
+    }
+    play(id)
+  })
+}
+
+function play(id) {
+  isRunning = true
+  const head = document.getElementById(id)
+  const colors = '#66CCFF #FFFF00 #9999FF #006666 #0080FF #53C5B9 #00FFCC #99FFFF #EE0000 #363636 #EE82EE'.split(' ')
+  const tarStr = 'LazySheep'
+  let str = ''
+  for (const ch of tarStr) {
+    str += '<span></span>'
+  }
+  head.innerHTML = str
+  const history = run(tarStr).history
+  history.forEach((text, i) => {
+    setTimeout(function() {
+      let j = 0
+      for (const ch of text) {
+        head.children[j].innerText = ch
+        if (ch !== tarStr[j]) {
+          let colorIndex = Math.floor(Math.random() * colors.length)
+          head.children[j].style.color = colors[colorIndex]
+        } else {
+          head.children[j].style.color = '#000'
+        }
+        j++
+      }
+      if (++i === history.length) {
+        isRunning = false
+      }
+    }, i * 80)
+  })
+}
+
+init('title')
